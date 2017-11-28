6433ee724926dfca754f1a7c1d0c211208dd773a
mormolis <george.lamprakis@gmail.com>
2017-11-28 16:23:25 +0000

    a day passes method moved to update quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,12 +10,13 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
       update_common_item(item) if !SPECIAL_ITEMS.include?(item.name)
       update_aged_brie(item) if item.name == SPECIAL_ITEMS[1]
       update_backstage_passes(item) if item.name == SPECIAL_ITEMS[2]
       update_conjured(item) if item.name == SPECIAL_ITEMS[3]
+      a_day_passes(item) if item.name != SPECIAL_ITEMS[0]
     end  
   end
 
@@ -26,25 +27,23 @@ class GildedRose
     elsif has_expired?(item) && item.quality > MIN_QUALITY
       decrease_quality_by(item, 2)
     end
-    a_day_passes(item)
+    
   end
 
   def update_aged_brie(item)
     increase_quality_by(item, 1) if has_expired?(item) && item.quality < MAX_QUALITY
     increase_quality_by(item, 1) if item.quality < MAX_QUALITY
-    a_day_passes(item)
+    
   end
 
   def update_backstage_passes(item)
     item.quality = MIN_QUALITY if has_expired?(item)
     increase_quality_by(item, 1) if more_than_ten_days_or_five_or_less_and_not_expired(item)
     increase_quality_by(item, 2) if item.sell_in <= BACKSTAGE_PASSES_VALUED_DAYS_MAX && has_not_expired?(item)
-    a_day_passes(item)
   end
 
   def update_conjured(item)
     update_common_item(item)
-    item.sell_in += 1
     update_common_item(item)
   end
 
